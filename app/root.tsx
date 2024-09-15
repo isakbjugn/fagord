deff3721d03a734815e17948a11e12f70de2c276
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2024-09-15 21:55:19 +0200

    Dobbelt opp med meta-tagger

--- app/root.tsx
+++ app/root.tsx
@@ -50,6 +50,10 @@ export default function Root() {
     <html lang="nb">
     <head>
       <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width; initial-scale=1; viewport-fit=cover" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       <Meta />
       <Links />
       <script src="https://kit.fontawesome.com/aff1df517b.js" crossOrigin="anonymous"></script>
