cf9a7c6846f51602f226ca43dbaa631643fbddb3
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2024-09-15 18:49:25 +0200

    Tar i bruk ny meta-tag for PWA på iOS-enheter

--- app/root.tsx
+++ app/root.tsx
@@ -37,7 +37,6 @@ export const links: LinksFunction = () => [
 export const meta: MetaFunction = () => [
   { title: 'Fagord' },
   { name: 'description', content: 'Fagord er din kilde til norske fagtermer.' },
-  { name: 'apple-mobile-web-app-capable', content: 'yes' },
 ];
 
 export default function Root() {
@@ -46,6 +45,7 @@ export default function Root() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <Meta />
         <Links />
         <script src="https://kit.fontawesome.com/aff1df517b.js" crossOrigin="anonymous"></script>
