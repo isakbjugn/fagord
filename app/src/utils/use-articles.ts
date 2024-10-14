741a2c02c245ee7f50e88ad141c56f2fe4fdb445
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2024-10-14 21:17:34 +0200

    Fjerner Artikler-side, og dermed siste del av fagord-main (SPA router)

--- app/src/utils/use-articles.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-
-import { fetchArticles } from '../lib/fetch';
-
-export const useArticles = () =>
-  useQuery({
-    queryKey: ['articles'],
-    queryFn: fetchArticles,
-  });
