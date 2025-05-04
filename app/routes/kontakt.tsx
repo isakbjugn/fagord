8166e3a3ae80c0e2953a61bf2e7e75860e39cc2a
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-05-04 19:49:17 +0200

    Skriver /kontakt uten @mui

--- app/routes/kontakt.tsx
+++ app/routes/kontakt.tsx
@@ -1,4 +1,3 @@
-import { Button, Card, CardActions, CardContent, Typography } from '@mui/material';
 import { Link } from '@remix-run/react';
 
 import { contactEmailUrl, fagordIssuesUrl, linkedInUrl } from '~/lib/constants';
@@ -13,72 +12,55 @@ export default function Kontakt() {
         ordboken og til nettsiden! Her er noen måter du kan bidra:
       </p>
       <section className={style.categories}>
-        <Card className={style.card}>
-          <CardContent>
-            <div className={style['card-header']}>
-              <i aria-hidden className="fa-solid fa-circle-plus fa-2xl" />
-            </div>
-            <Typography gutterBottom variant="h5" color="white">
-              Bidra til ordboken
-            </Typography>
-            <ul style={{ textAlign: 'start' }}>
-              <li>Legg ord inn i termbasen</li>
-              <li>Foreslå oversettelser til eksisterende termer</li>
-              <li>Legg inn definisjoner på termer</li>
-            </ul>
-          </CardContent>
-          <CardActions style={{ justifyContent: 'center' }}>
-            <Link to="/ny-term" style={{ textDecoration: 'none' }}>
-              <Button variant="contained">Legg til ny term</Button>
-            </Link>
-          </CardActions>
-        </Card>
-        <Card className={style.card}>
-          <CardContent>
-            <div className={style['card-header']}>
-              <i aria-hidden className="fa-brands fa-github fa-2xl" />
-            </div>
-            <Typography gutterBottom variant="h5" color="white">
-              Vær med og utvikle
-            </Typography>
-            <ul style={{ textAlign: 'start' }}>
-              <li>Legg inn oppgaver til utvikling på GitHub</li>
-              <li>Opplever du feil? Dette kan også legges inn på GitHub</li>
-              <li>Vi tar også imot kodeforslag!</li>
-            </ul>
-          </CardContent>
-          <CardActions style={{ justifyContent: 'center' }}>
-            <a href={fagordIssuesUrl} style={{ textDecoration: 'none' }}>
-              <Button variant="contained">Åpne GitHub</Button>
+        <div className={style.card}>
+          <div className={style.cardHeader}>
+            <i aria-hidden className="fa-solid fa-circle-plus fa-2xl" />
+          </div>
+          <h4>Bidra til ordboken</h4>
+          <ul style={{ textAlign: 'start' }}>
+            <li>Legg ord inn i termbasen</li>
+            <li>Foreslå oversettelser til eksisterende termer</li>
+            <li>Legg inn definisjoner på termer</li>
+          </ul>
+          <Link className="btn btn-fagord-blue btn-lg" to="/ny-term" style={{ textDecoration: 'none' }}>
+            Legg til ny term
+          </Link>
+        </div>
+        <div className={style.card}>
+          <div className={style.cardHeader}>
+            <i aria-hidden className="fa-brands fa-github fa-2xl" />
+          </div>
+          <h4>Vær med og utvikle</h4>
+          <ul style={{ textAlign: 'start' }}>
+            <li>Legg inn oppgaver til utvikling på GitHub</li>
+            <li>Opplever du feil? Dette kan også legges inn på GitHub</li>
+            <li>Vi tar også imot kodeforslag!</li>
+          </ul>
+          <a className="btn btn-fagord-blue btn-lg" href={fagordIssuesUrl} style={{ textDecoration: 'none' }}>
+            Åpne GitHub
+          </a>
+        </div>
+        <div className={style.card}>
+          <div className={style.cardHeader}>
+            <i aria-hidden className="fa-solid fa-envelope fa-2xl" />
+          </div>
+          <h4>Kom i kontakt med oss</h4>
+          <p className={style.smallParagraph}>
+            Vi tar også gjerne imot direktehenvendelser, enten om du har andre tanker om Fagord eller bare vil slå av en
+            prat:
+          </p>
+          <p className={style.smallParagraph}>
+            E-post:{' '}
+            <a className={style.link} href={contactEmailUrl}>
+              isakbjugn@gmail.com
             </a>
-          </CardActions>
-        </Card>
-        <Card className={style.card}>
-          <CardContent>
-            <div className={style['card-header']}>
-              <i aria-hidden className="fa-solid fa-envelope fa-2xl" />
-            </div>
-            <Typography gutterBottom variant="h5" color="white">
-              Kom i kontakt med oss
-            </Typography>
-            <Typography variant="body2" color="white" sx={{ mb: 2 }}>
-              Vi tar også gjerne imot direktehenvendelser, enten om du har andre tanker om Fagord eller bare vil slå av
-              en prat:
-            </Typography>
-            <Typography variant="body2" color="white">
-              E-post:{' '}
-              <a className={style.link} href={contactEmailUrl}>
-                isakbjugn@gmail.com
-              </a>
-            </Typography>
-            <Typography variant="body2" color="white">
-              LinkedIn:{' '}
-              <a className={style.link} href={linkedInUrl}>
-                in/isakbjugn
-              </a>
-            </Typography>
-          </CardContent>
-        </Card>
+            <br />
+            LinkedIn:{' '}
+            <a className={style.link} href={linkedInUrl}>
+              in/isakbjugn
+            </a>
+          </p>
+        </div>
       </section>
       <section>
         <p>
