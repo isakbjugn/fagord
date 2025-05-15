e08c1b0b1507457f176dca7c3b9e613c2010b073
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-05-15 15:23:15 +0200

    Fjerner «Gå til side»

--- app/lib/components/paginator.tsx
+++ app/lib/components/paginator.tsx
@@ -14,20 +14,6 @@ export const Paginator = ({ table }: Props) => {
           {table.getState().pagination.pageIndex + 1} av {table.getPageCount().toLocaleString()}
         </strong>
       </span>
-      <span className="flex items-center gap-1">
-        | Gå til side:
-        <input
-          type="number"
-          min="1"
-          max={table.getPageCount()}
-          defaultValue={table.getState().pagination.pageIndex + 1}
-          onChange={(e) => {
-            const page = e.target.value ? Number(e.target.value) - 1 : 0;
-            table.setPageIndex(page);
-          }}
-          className="border p-1 rounded w-16"
-        />
-      </span>
       <select
         className="border p-1 rounded"
         value={table.getState().pagination.pageSize}
