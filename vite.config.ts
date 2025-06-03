35f53135f3d302784905e1817e2e41a319836d7b
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-06-03 21:04:24 +0200

    Tar i bruk alle future-flagg, egen konfig for filbasert ruting

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,12 @@ export default defineConfig({
     remix({
       ignoredRouteFiles: ['**/*.css'],
       future: {
+        v3_fetcherPersist: true,
+        v3_lazyRouteDiscovery: true,
+        v3_relativeSplatPath: true,
+        v3_routeConfig: true,
         v3_singleFetch: true,
+        v3_throwAbortReason: true,
       },
     }),
     tsconfigPaths(),
