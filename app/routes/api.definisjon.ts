5009371dfcfedda3c1a064827ee64b8fef564214
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-09-30 20:30:36 +0200

    Ny URL til definisjoner fra Rust-API

--- app/routes/api.definisjon.ts
+++ app/routes/api.definisjon.ts
@@ -2,7 +2,7 @@ import type { Route } from './+types/api.definisjon';
 
 export async function clientAction({ request }: Route.ClientActionArgs): Promise<string | null> {
   const formData = await request.formData();
-  const definitionApiUrl = 'https://rust-api.fagord.no';
+  const definitionApiUrl = 'https://rust-api.fagord.no/definitions';
   const { term } = Object.fromEntries(formData) as { term: string };
 
   const res = await fetch(`${definitionApiUrl}/${term}`);
