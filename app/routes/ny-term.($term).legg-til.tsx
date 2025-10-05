948e631a2bbf18db17e0134fca3abe48c94617eb
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-10-05 17:28:31 +0200

    Fjerner tomme strenger før term opprettes

--- app/routes/ny-term.($term).legg-til.tsx
+++ app/routes/ny-term.($term).legg-til.tsx
@@ -5,6 +5,8 @@ import type { Route } from './+types/ny-term.($term).legg-til';
 
 export const action = async ({ request }: Route.ActionArgs) => {
   const formData = await request.formData();
+  const createTerm = Object.fromEntries(Object.entries(Object.fromEntries(formData)).filter(([_, v]) => v !== ''));
+
   const FAGORD_RUST_API_URL = process.env.FAGORD_RUST_API_DOMAIN || 'http://localhost:8080';
   const termsApiUrl = `${FAGORD_RUST_API_URL}/terms`;
   const newTerm: Term = await fetch(termsApiUrl, {
@@ -13,7 +15,7 @@ export const action = async ({ request }: Route.ActionArgs) => {
       'Content-Type': 'application/json',
     },
     method: 'POST',
-    body: JSON.stringify(Object.fromEntries(formData)),
+    body: JSON.stringify(createTerm),
   }).then((res) => {
     if (!res.ok) {
       throw Error(res.status.toString() + ' ' + res.statusText);
