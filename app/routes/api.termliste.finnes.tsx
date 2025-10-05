7a054f1baf6791fd96d7d304e3833de2942b9e4b
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-10-05 13:38:35 +0200

    Bruker Rust-API til å spørre om term finnes

--- app/routes/api.termliste.finnes.tsx
+++ app/routes/api.termliste.finnes.tsx
@@ -1,22 +1,22 @@
-import type { Term } from '~/types/term';
 import type { Route } from './+types/api.termliste.finnes';
 
-export async function clientAction({ request }: Route.ClientActionArgs) {
+export async function action({ request }: Route.ActionArgs) {
   const formData = await request.formData();
   const { term } = Object.fromEntries(formData) as { term: string };
-  const cachedTerms = localStorage.getItem('terms');
-  if (term && cachedTerms) {
-    const terms = JSON.parse(cachedTerms) as Term[];
-    const exists = terms.find((cachedTerm) => cachedTerm.en === term) !== undefined;
-    const validationText = exists ? 'Termen finnes allerede i termlista.' : undefined;
+  const FAGORD_RUST_API_URL = import.meta.env.FAGORD_RUST_API_DOMAIN || 'http://localhost:8080';
+  const termsUrl = `${FAGORD_RUST_API_URL}/term-stats/exists/${term}`;
 
+  const termResponse = await fetch(termsUrl);
+  if (!termResponse.ok) {
     return {
-      exists,
-      validationText,
+      exists: undefined,
     };
   }
+  const { exists } = await termResponse.json();
+  const validationText = exists ? 'Termen finnes allerede i termlista.' : undefined;
 
   return {
-    exists: false,
+    exists,
+    validationText,
   };
 }
