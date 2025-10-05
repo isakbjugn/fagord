e6536ba33f96571b9325cce91c19575b208384ad
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-10-05 17:15:19 +0200

    Henter hele termlisten fra Rust-API

--- app/routes/termliste/route.tsx
+++ app/routes/termliste/route.tsx
@@ -5,42 +5,20 @@ import Table from './table';
 import { Loader } from '~/lib/components/loader';
 import { ErrorMessage } from '~/lib/components/error-message';
 import { Term } from '~/types/term';
-import { Route } from './+types/route';
 
-export const loader = () => {
-  const termsUrl = 'https://api.fagord.no/termer/';
-  const terms = fetch(termsUrl).then(async (res) => {
-    if (!res.ok) {
-      throw new Response('Klarte ikke å hente termer', { status: 500 });
-    }
-    return (await res.json()) as Term[];
-  });
+export async function loader() {
+  const FAGORD_RUST_API_URL = process.env.FAGORD_RUST_API_DOMAIN || 'http://localhost:8080';
+  const termsApiUrl = `${FAGORD_RUST_API_URL}/terms`;
+  const termsResponse = await fetch(termsApiUrl);
 
-  return {
-    terms,
-  };
-};
-
-export const clientLoader = ({ serverLoader }: Route.ClientLoaderArgs) => {
-  const cachedTerms = localStorage.getItem('terms');
-  if (cachedTerms) {
-    return {
-      terms: JSON.parse(cachedTerms) as Term[],
-    };
+  if (!termsResponse.ok) {
+    throw new Response('Klarte ikke å hente termer', { status: 500 });
   }
-
-  return (serverLoader() as Promise<{ terms: Promise<Term[]> }>).then((data) => {
-    data.terms.then((terms) => {
-      localStorage.setItem('terms', JSON.stringify(terms));
-    });
-    return data;
-  });
-};
-
-clientLoader.hydrate = true;
+  return (await termsResponse.json()) as Term[];
+}
 
 export default function Termliste() {
-  const { terms } = useLoaderData<typeof loader>();
+  const terms = useLoaderData<typeof loader>();
   return (
     <Suspense fallback={<Loader />}>
       <Await resolve={terms}>{(resolvedTerms) => <Table terms={resolvedTerms} />}</Await>
