09d5ad84c13e4df3486717bf75ae6a82a8e114c9
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-10-05 17:52:37 +0200

    Rydder i (ubrukte) typer

--- app/routes/term.$termId.varianter.legg-til.tsx
+++ app/routes/term.$termId.varianter.legg-til.tsx
@@ -1,12 +1,12 @@
 import { redirect } from 'react-router';
 
-import type { SubmitVariant } from '~/types/term';
+import type { CreateVariant } from '~/types/term';
 import type { Route } from './+types/term.$termId.varianter.legg-til';
 
 export async function action({ request, params }: Route.ActionArgs) {
   const formData = await request.formData();
   const { termId } = params;
-  const submitVariant = Object.fromEntries(formData) as unknown as SubmitVariant;
+  const submitVariant = Object.fromEntries(formData) as unknown as CreateVariant;
   const FAGORD_RUST_API_URL = process.env.FAGORD_RUST_API_DOMAIN || 'http://localhost:8080';
   const termsApiUrl = `${FAGORD_RUST_API_URL}/terms/${termId}/variants`;
 
