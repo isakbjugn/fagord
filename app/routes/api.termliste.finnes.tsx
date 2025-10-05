0a20bfa50c5ccf8167db229afbcad082f3c2d874
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-10-05 13:49:07 +0200

    Bruker process.env i server-kode

--- app/routes/api.termliste.finnes.tsx
+++ app/routes/api.termliste.finnes.tsx
@@ -3,7 +3,7 @@ import type { Route } from './+types/api.termliste.finnes';
 export async function action({ request }: Route.ActionArgs) {
   const formData = await request.formData();
   const { term } = Object.fromEntries(formData) as { term: string };
-  const FAGORD_RUST_API_URL = import.meta.env.FAGORD_RUST_API_DOMAIN || 'http://localhost:8080';
+  const FAGORD_RUST_API_URL = process.env.FAGORD_RUST_API_DOMAIN || 'http://localhost:8080';
   const termsUrl = `${FAGORD_RUST_API_URL}/term-stats/exists/${term}`;
 
   const termResponse = await fetch(termsUrl);
