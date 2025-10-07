7da04615865e892fa80dc071af755dad1e0aa7a9
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-10-07 20:58:11 +0200

    Sender fagfelt fra termliste-loader

--- app/routes/termliste/subject-filter/subject-filter.tsx
+++ app/routes/termliste/subject-filter/subject-filter.tsx
@@ -1,44 +1,37 @@
 import style from './subject-filter.module.css';
-import { AllSubjects } from '~/types/subject';
-import { useSubjectsFetcher } from '~/routes/termliste/subject-filter/use-subjects-fetcher';
+import { AllSubjects, Subject } from '~/types/subject';
+import { Suspense } from 'react';
+import { Await } from 'react-router';
 
 type Props = {
+  subjects: Promise<Subject[]>;
   onChange: (subject: string) => void;
 };
 
-export const SubjectFilter = ({ onChange }: Props) => {
-  const subjectsFetcher = useSubjectsFetcher();
-
-  function handleClick() {
-    subjectsFetcher.load('/api/fagfelt');
-  }
-
-  if (subjectsFetcher.state === 'loading' && !subjectsFetcher.data)
-    return (
-      <div className={style.skeleton}>
-        <p>
-          <i className="fa-solid fa-spinner fa-spin" /> Henter fagfelt
-        </p>
-      </div>
-    );
-
-  if (subjectsFetcher.data?.error) {
-    return (
-      <button className={style.subjects} onClick={handleClick}>
-        <i className="fa-solid fa-rotate"></i> {subjectsFetcher.data.message}
-      </button>
-    );
-  }
-
-  if (subjectsFetcher.data) {
-    return (
-      <select className={style.subjects} onChange={(event) => onChange(event.currentTarget.value)}>
-        {[AllSubjects, ...subjectsFetcher.data.subjects].map((subject) => (
-          <option key={subject.name} value={subject.name}>
-            {subject.name}
-          </option>
-        ))}
-      </select>
-    );
-  }
+export const SubjectFilter = ({ onChange, subjects }: Props) => {
+  return (
+    <Suspense fallback={<Loader />}>
+      <Await resolve={subjects}>
+        {(subjects) => (
+          <select className={style.subjects} onChange={(event) => onChange(event.currentTarget.value)}>
+            {[AllSubjects, ...subjects].map((subject) => (
+              <option key={subject.name} value={subject.name}>
+                {subject.name}
+              </option>
+            ))}
+          </select>
+        )}
+      </Await>
+    </Suspense>
+  );
 };
+
+function Loader() {
+  return (
+    <div className={style.skeleton}>
+      <p>
+        <i className="fa-solid fa-spinner fa-spin" /> Henter fagfelt
+      </p>
+    </div>
+  );
+}
