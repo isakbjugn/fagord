77eda19dda42c3b95c4bd3802402a1afd0515ebe
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-10-09 09:59:53 +0200

    Første test på termliste

---
+++ test/routes/termliste.test.tsx
@@ -0,0 +1,30 @@
+import { createRoutesStub } from 'react-router';
+import Termliste from '~/routes/termliste';
+import { cleanup, render, screen, waitFor } from '@testing-library/react';
+import { afterEach, describe, test } from 'vitest';
+import { createValidTerms } from '../test-data/term';
+
+afterEach(cleanup);
+
+describe('Tester innhold på og navigasjon fra Termliste-siden', () => {
+  test('Termlisten inneholder engelske termer og norske oversettelser', async () => {
+    const Stub = createRoutesStub([
+      {
+        path: '/termliste',
+        Component: Termliste,
+        loader() {
+          return {
+            terms: createValidTerms(),
+            subjects: [],
+          };
+        },
+      },
+    ]);
+
+    render(<Stub initialEntries={['/termliste']} />);
+
+    await waitFor(() => screen.findByText('cortex'));
+    await waitFor(() => screen.findByText('hjernebark'));
+    await waitFor(() => screen.findByText('hjernebork'));
+  });
+});
