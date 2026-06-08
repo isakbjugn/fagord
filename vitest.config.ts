7d701b549b68c5ff2d584297165f27b6a33ed48b
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2026-06-08 19:55:53 +0200

    tsconfigPaths er nå innebygd i Vite

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  resolve: {
+    tsconfigPaths: true,
+  },
   test: {
     environment: 'happy-dom',
   },
